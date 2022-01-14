36295cabf2cdab4ed68fa3b907c7b467101a02a5
ZeroCho <zerohch0@gmail.com>
2022-01-14 16:33:26 +0900

    Update: 패키지이름변경

--- ch4/android/app/src/main/java/com/zerocho/fooddeliveryapp/MainActivity.java
+++ ch4/android/app/src/main/java/com/zerocho/fooddeliveryapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.fooddeliveryapp;
+package com.zerocho.fooddeliveryapp;
 
 import com.facebook.react.ReactActivity;
 import android.os.Bundle;
--- ch4/android/app/src/main/java/com/zerocho/fooddeliveryapp/MainApplication.java
+++ ch4/android/app/src/main/java/com/zerocho/fooddeliveryapp/MainApplication.java
@@ -1,7 +1,9 @@
-package com.fooddeliveryapp;
+package com.zerocho.fooddeliveryapp;
 
 import android.app.Application;
 import android.content.Context;
+
+import com.facebook.react.BuildConfig;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
@@ -62,7 +64,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.fooddeliveryapp.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.zerocho.fooddeliveryapp.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
