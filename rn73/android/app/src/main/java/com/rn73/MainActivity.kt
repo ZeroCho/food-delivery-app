62a7b5b6a66c294380badfd26492bd2b5330f4e6
zerocho <zerohch0@gmail.com>
2024-09-19 22:29:44 +0900

    add: initial rn75

--- rn73/android/app/src/main/java/com/rn73/MainActivity.kt
+++ rn73/android/app/src/main/java/com/rn73/MainActivity.kt
@@ -5,6 +5,7 @@ import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen;
 
 class MainActivity : ReactActivity() {
 
@@ -14,6 +15,7 @@ class MainActivity : ReactActivity() {
    */
   override fun getMainComponentName(): String = "rn73"
   override fun onCreate(savedInstanceSate: Bundle?) {
+    SplashScreen.show(this);
     super.onCreate(null);
   }
 
